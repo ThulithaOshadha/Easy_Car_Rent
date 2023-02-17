fdc6f5ea356c066e59445e81bdd8f4171767ecf4
ThulithaOshadha <thulithaoshadha205@gmail.com>
2023-02-17 09:01:57 +0530

    add driver entity and car entity

--- src/main/java/lk/easycar/entity/Car.java
+++ src/main/java/lk/easycar/entity/Car.java
@@ -1,4 +1,29 @@
 package lk.easycar.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Car {
+    @Id
+    private String vehicleNum;
+    private String model;
+    private String VehicleType;
+    private String TransmissionType;
+    private String color;
+    private String FuelType;
+    private int passengers;
+    private BigDecimal monthlyPrice;
+    private BigDecimal dailyPrice;
+    private int freeMileage;
+    private boolean available;
 }
---
+++ src/main/java/lk/easycar/entity/Driver.java
@@ -0,0 +1,22 @@
+package lk.easycar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Driver {
+    @Id
+    private String license;
+    private String name;
+    private String address;
+    private String contact;
+}
