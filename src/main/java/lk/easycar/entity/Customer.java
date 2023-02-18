6f0d15c7798e3fab5dfe112a446d256469004844
ThulithaOshadha <thulithaoshadha205@gmail.com>
2023-02-18 12:37:36 +0530

    add customer entity

---
+++ src/main/java/lk/easycar/entity/Customer.java
@@ -0,0 +1,24 @@
+package lk.easycar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Id;
+import javax.persistence.Lob;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Customer {
+    @Id
+    private String nic;
+    private String name;
+    private String address;
+    private String license;
+    private String contact;
+    private String password;
+    @Lob
+    private byte[] nicImage;
+}
