8a3d1ee9af23b753041ecd6a961685f4abaf0153
ThulithaOshadha <thulithaoshadha205@gmail.com>
2023-02-19 01:02:31 +0530

    table update

--- src/main/java/lk/easycar/controller/CustomerController.java
+++ src/main/java/lk/easycar/controller/CustomerController.java
@@ -6,6 +6,8 @@ import lk.easycar.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/customer")
 @CrossOrigin
@@ -18,4 +20,19 @@ public class CustomerController {
         service.saveCustomer(dto);
         return new ResponseUtil("ok","successfully registered",null);
     }
+    @GetMapping
+    public ResponseUtil getAllCustomer(){
+        return new ResponseUtil("ok","successfully loaded",service.getAllCustomers());
+    }
+    @DeleteMapping
+    public ResponseUtil deleteCustomer(@RequestParam String id){
+        service.deleteCustomer(id);
+        return new ResponseUtil("ok","successfully deleted",null);
+    }
+    @PutMapping
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto){
+        service.updateCustomer(dto);
+        return new ResponseUtil("ok","successfully updated",null);
+    }
+
 }
