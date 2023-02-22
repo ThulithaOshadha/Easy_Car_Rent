a5859ff312d96a8a9d363256483aae35a905b6f8
ThulithaOshadha <thulithaoshadha205@gmail.com>
2023-02-23 00:40:46 +0530

    add booking service package

---
+++ src/main/java/lk/easycar/service/BookingService.java
@@ -0,0 +1,7 @@
+package lk.easycar.service;
+
+import lk.easycar.dto.BookingDTO;
+
+public interface BookingService {
+    public void addBooking(BookingDTO dto);
+}
---
+++ src/main/java/lk/easycar/service/impl/BookingServiceImpl.java
@@ -0,0 +1,19 @@
+package lk.easycar.service.impl;
+
+import lk.easycar.dto.BookingDTO;
+import lk.easycar.entity.Booking;
+import lk.easycar.repo.BookingRepo;
+import lk.easycar.service.BookingService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookingServiceImpl implements BookingService {
+    @Autowired
+    private BookingRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+    @Override
+    public void addBooking(BookingDTO dto) {
+        repo.save(mapper.map(dto, Booking.class));
+    }
+}
